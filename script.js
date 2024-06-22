97a76ab3d3f7eb86576749bb88ca11406f2dd9fb
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-06-22 17:21:27 +0300

    Create function for the player choice
    It will allow the computer to choose between rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hello World")
 
-
+// computer choice
 // random value from 0 to 1 
-// based on the value select rock, paper or scissors for the computer 
+// based on the value, choose rock, paper or scissors for the computer 
 // if random value (equal or less 0.33 = rock) (equal or less 0.66 = paper) (else scissors)
 let computerChoice;
 
@@ -18,5 +18,16 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// player choice
+// ask the player what he want to choose rock, paper or scissors
+// store the player option in a variable 
+// 
 
+function getHumanChoice() {
+    const playerChoice = prompt("message"); 
+    return playerChoice; 
+}
 
+// players scores 
+let humanScore = 0; 
+let computerScore = 0;
